2258b59277c7eb2182716d317afd7a7482e448bb
CK-Aashlesh <AASHLESHKUMAR87@GMAIL.COM>
2025-04-02 21:06:56 +0530

    add

---
+++ script.js
@@ -0,0 +1,82 @@
+<<<<<<< HEAD
+
+function Message(){
+    console.log("Files Not Uploaded");
+    alert("Files Not Uploaded")
+}
+
+
+// function SendMail(){
+//     let parms = {
+//     from_name: document.getElementById("fullName").value,
+//     email_id: document.getElementById("email_id").value,
+//     message: document.getElementById("message").value,}
+//     emailjs.send("service_18waiwa","template_4o1oamm" , parms).then(alert("Email Send!!"));
+// }       
+
+// document.querySelectorAll("nav ul li a").forEach(link => {
+//     if (link.href === window.location.href) {
+//         link.classList.add("active");
+//     }
+// });
+function SendMail() {
+    let parms = {
+        from_name: document.getElementById("fullName").value,
+        email_id: document.getElementById("emailid").value,
+        message: document.getElementById("message").value,
+    };
+
+    emailjs.send("service_18waiwa", "template_4o1oamm", parms)
+        .then(response => {
+            alert("Email Sent Successfully!");
+            console.log("SUCCESS!", response);
+        })
+        .catch(error => {
+            alert("Failed to send email. Please try again.");
+            console.error("ERROR:", error);
+        });
+}
+
+=======
+// alert("Work in Progress!!");
+
+
+
+// function Message(){
+//     console.log("Files Not Uploaded");
+//     alert("Files Not Uploaded")
+// }
+
+
+// function SendMail(){
+//     let parms = {
+//     from_name: document.getElementById("fullName").value,
+//     email_id: document.getElementById("email_id").value,
+//     message: document.getElementById("message").value,}
+//     emailjs.send("service_18waiwa","template_4o1oamm" , parms).then(alert("Email Send!!"));
+// }       
+
+// document.querySelectorAll("nav ul li a").forEach(link => {
+//     if (link.href === window.location.href) {
+//         link.classList.add("active");
+//     }
+// });
+function SendMail() {
+    let parms = {
+        from_name: document.getElementById("fullName").value,
+        emailid: document.getElementById("email_id").value,
+        message: document.getElementById("message").value,
+    };
+
+    emailjs.send("service_18waiwa", "template_4o1oamm", parms)
+        .then(response => {
+            alert("Email Sent Successfully!");
+            console.log("SUCCESS!", response);
+        })
+        .catch(error => {
+            alert("Failed to send email. Please try again.");
+            console.error("ERROR:", error);
+        });
+}
+
+>>>>>>> f43706e9714d4aa63061d6370c4405722da72e3c
